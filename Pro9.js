f8d3a4841187beb611272a0d5dfb422e2c989aa6
DenR <den.ran@tutanota.com>
2018-05-07 09:32:12 -0400

    5-7-18_Work

--- Pro9.js
+++ Pro9.js
@@ -4,10 +4,13 @@ function Pro9()
     var todayDate = new Date();
     console.log("today's date :" + todayDate );
 
-    var xmasDate = new Date(todayDate.getFullYear(),12-1,25);
+    //var xmasDate = new Date(todayDate.getFullYear(),12-1,25);
+    var xmasDate = new Date(todayDate.getFullYear(),4,6);
     console.log("Xmas date :" + xmasDate );
 
-    var dateDiff = Math.floor((((xmasDate - todayDate/ 1000*60*60*24);
+    //var dateDiff = Math.floor(xmasDate - todayDate/ 1000*60*60*24);
+    
+    var dateDiff = xmasDate- todayDate;
     console.log("Days in Christmas :" + dateDiff );
 }
 
