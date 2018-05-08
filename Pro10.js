97e184ed3e3f693f45a515bb8c72110c776be428
HeRann <hemal.m.rana@outlook.com>
2018-05-07 20:56:00 -0400

    5-7-18(2)_Home

---
+++ Pro10.js
@@ -0,0 +1,23 @@
+////Write a JavaScript program to calculate multiplication and division of two numbers (input from user). 
+function Pro10()
+{
+var num1;
+var num2;
+var resultMulti;
+var resultDivi;
+
+funtion number: multi(num1, num2)
+{
+    resultMulti = num1* num2;
+}
+
+function number: divi()
+{
+    resultDivi= num1/num2;
+}
+
+}
+
+Pro10();
+
+////Actual Code
