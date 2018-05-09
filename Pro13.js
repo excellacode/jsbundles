ec83c04903218b22fb278d8aa95f2b069c284ac2
DenR <den.ran@tutanota.com>
2018-05-09 15:22:56 -0400

    5-9-18(2)_Work

--- Pro13.js
+++ Pro13.js
@@ -1,1 +1,28 @@
 //// Write a JavaScript exercise to create a variable using a user-defined name
+function Pro13()
+{
+    //Case : 1
+    var var_name = "Hello! World";
+    var num = 120;
+    var_name = num;
+    console.log(var_name)
+
+    ////Case : 2
+    // var string1 = "Hello World";
+    // function sayHello(){
+    //   return string1;
+    // }
+    // console.log(sayHello());
+    // sayHello();
+}
+
+Pro13();
+
+
+
+
+////Actual Code: 
+// var var_name = 'abcd';
+// var n = 120;
+// this[var_name] = n;
+// console.log(this[var_name])
---
+++ Pro14.js
@@ -0,0 +1,13 @@
+////Write a JavaScript exercise to get the extension of a filename
+
+function Pro14()
+{
+
+
+}
+
+Pro14();
+
+
+
+////Actual Code: 
