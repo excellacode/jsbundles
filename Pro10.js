e9b63c35cea2b9c954dd3e91b7f0ad5f0bd92246
DenR <den.ran@tutanota.com>
2018-05-11 15:54:02 -0400

    5-11-18_Work

--- Pro10.js
+++ Pro10.js
@@ -1,30 +1,88 @@
 ////Write a JavaScript program to calculate multiplication and division of two numbers (input from user). 
-<p id="funct"></p>
-
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Pro10 - Write a JavaScript program to calculate multiplication and division of two numbers (input from user). </title>
 <script>
-
-function Pro10(num1,num2)
-{
-    //var num1
-    //var num2
-    //var resultMulti
-    //var resultDivi
-
-    var resultMulti = multi(1,2);
-    funtion multi (num1, num2)
+    function multi()
     {
-        resultMulti = num1* num2
-    }
+        var inputOne = document.getElementById("txtoneid").value;
+        var inputTwo = document.getElementById("txttwoid").value;
+        if(isNaN(inputOne) == true || isNaN(inputTwo) == true)
+        {
+            var result = "Operation invalid.";
+        }
+        else 
+        {
+            var result = inputOne * inputTwo;
+        }
 
-    var resultDivi = divi(1,2);
-    function divi (num1, num2)
+        document.getElementById("lblResult").innerHTML = result;
+    }
+    function divi()
     {
-        resultDivi= num1/num2
-    }
+        var inputOne = document.getElementById("txtoneid").value;
+        var inputTwo = document.getElementById("txttwoid").value;
 
-}
+        if(isNaN(inputOne) == true || isNaN(inputTwo) == true || inputTwo == 0 )
+        {
+            var result = "Operation invalid.";
+        }
+        else
+        {
+            result = inputOne / inputTwo; 
+        }
+        document.getElementById("lblResult").innerHTML = result;
+    }
 </script>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <td>
+                <label> 1st Number : </label>
+            </td>
+            <td>
+                <input type="text" name="txtNumOne" value="" id="txtoneid" />
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label> 2nd Number : </label>
+            </td>
+            <td>
+                <input type="text" name="txtNumTwo" value="" id="txttwoid" />
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="button" name="btnMultiple" value="Multiply" onclick="multi()"/>
+            </td>
+            <td>
+                <input type="button" name="btnDivide" value="Divide" onclick="divi()" />
+            </td>
+        </tr>
+    </table>
+    The Result is :  
+     <label id="lblResult"></label> 
+</body>
+</html>
 
 //document.getElementById("funct").innerHTML = Pro10();
 
 ////Actual Code:
+// function multiplyBy()
+// {
+//         num1 = document.getElementById("firstNumber").value;
+//         num2 = document.getElementById("secondNumber").value;
+//         document.getElementById("result").innerHTML = num1 * num2;
+// }
+
+// function divideBy() 
+// { 
+//         num1 = document.getElementById("firstNumber").value;
+//         num2 = document.getElementById("secondNumber").value;
+// document.getElementById("result").innerHTML = num1 / num2;
+// }
